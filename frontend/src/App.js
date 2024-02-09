d3a409b03d90b2d1b90c3264fff1ec18d1fbb6e7
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-02-08 21:53:20 -0500

    polling and updating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,29 +48,35 @@ function App() {
     );
 
      setColors(colorCodes[i])
-  }, []);
+  }, [data]);
 
   useEffect(() => {
-    const data = window.localStorage.getItem("data");
-    if (data) {
-      setData(JSON.parse(data));
-      const currentTime = moment.utc();
-      const requestTime = moment.utc(JSON.parse(data).timestamp);
-      if (
-        config.URL === config.DEV || currentTime.hour() !== requestTime.hour() ||
-        !currentTime.isSame(requestTime, "date")
-      ) {
+    const fetchData = async () => {
+      const data = window.localStorage.getItem("data");
+      if (data) {
+        setData(JSON.parse(data));
+        const currentTime = moment.utc();
+        const requestTime = moment.utc(JSON.parse(data).timestamp);
+        if (
+          config.URL === config.DEV || currentTime.hour() !== requestTime.hour() ||
+          !currentTime.isSame(requestTime, "date")
+        ) {
+          axios.get(config.URL).then((res) => {
+            window.localStorage.setItem("data", JSON.stringify(res.data));
+            setData(res.data);
+          });
+        }
+      } else {
         axios.get(config.URL).then((res) => {
           window.localStorage.setItem("data", JSON.stringify(res.data));
           setData(res.data);
         });
       }
-    } else {
-      axios.get(config.URL).then((res) => {
-        window.localStorage.setItem("data", JSON.stringify(res.data));
-        setData(res.data);
-      });
-    }
+    };
+    fetchData();
+
+    const interval = setInterval(fetchData, 60000);
+    return () => clearInterval(interval);
   }, []);
 
   const currentDate = new Date();
