1be93f9d7f216e1cae55ffb33291dc63928966bc
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-02-08 21:24:35 -0500

    basic popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,8 +48,6 @@ function App() {
     );
 
      setColors(colorCodes[i])
-
-
   }, []);
 
   useEffect(() => {
@@ -97,8 +95,8 @@ function App() {
               style={{ color: "white", fontSize: "90px" }}
             >
               <b>
-                {/* Good {timeOfDay} <Name /> */}
-                Good {timeOfDay} <EditableLabel />
+                Good {timeOfDay} <Name />
+                {/* Good {timeOfDay} <EditableLabel /> */}
               </b>
             </h1>
             <h1
--- frontend/src/components/Name.js
+++ frontend/src/components/Name.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 function Name() {
   const [text, setText] = useState(window.localStorage.getItem("name") || "[name]");
+  const [inputValue, setInputValue] = useState(window.localStorage.getItem("name"));
+  const [showPopup, setShowPopup] = useState(false);
   
   window.onload = () => {
     if (window.localStorage.getItem("cancelCount") == null) {
@@ -10,15 +12,8 @@ function Name() {
     let cancelCount = parseInt(window.localStorage.getItem("cancelCount"));
     let currentName = window.localStorage.getItem("name");
     if ((currentName == null || currentName === "") && cancelCount < 2) {
-        const name = prompt("Please enter your name:");
-        if (name == null || name === "") {
-            cancelCount += 1
-            window.localStorage.setItem("cancelCount", cancelCount);
-        } else {
-            window.localStorage.setItem("name", name);
-            window.localStorage.setItem("cancelCount", 0);
-            setText(name);
-        }
+        setShowPopup(true);
+        
     }
     if (cancelCount >= 2) {
         window.localStorage.setItem("name", "");
@@ -26,9 +21,46 @@ function Name() {
     }
   };
 
+  const handleInputChange = (e) => {
+      setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      const name = inputValue;
+      let cancelCount = window.localStorage.getItem("cancelCount") || 0
+      if (name == null || name === "") {
+          cancelCount += 1
+          window.localStorage.setItem("cancelCount", cancelCount);
+      } else {
+          window.localStorage.setItem("name", name);
+          window.localStorage.setItem("cancelCount", 0);
+          setText(name);
+          setShowPopup(false);
+      }
+  };
+
   return (
     <span>
-      {text}
+      <span style={{textDecoration: "underline white", 
+                  textDecorationThickness: 3, textUnderlineOffset: 8}}
+        onClick={() => setShowPopup(true)}
+      >
+          {text}
+      </span>
+      {/* <img alt="" style={{ width: 24, marginLeft: 4, marginTop: 54}} src={Pencil} /> */}
+      {/* <span style={{fontSize: 24, marginLeft: 8}}>✏️</span> */}
+      {showPopup && <div className="popup">
+        <form onSubmit={handleSubmit}>
+          <label>Please enter your name:&nbsp;&nbsp;</label>
+          <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>}
     </span>
   );
 }
