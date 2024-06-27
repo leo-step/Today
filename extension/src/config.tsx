531f6158bc9a616573ba77f324c9b50d2fe30dec
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-06-26 21:59:05 -0700

    config for dev

--- extension/src/config.tsx
+++ extension/src/config.tsx
@@ -1,6 +1,6 @@
 const config = {
-  URL: "https://today-nujm46x7ta-ue.a.run.app",
-  DEV: "http://127.0.0.1:5000",
+  URL: "http://127.0.0.1:8000",
+  DEV: "http://127.0.0.1:8000",
   PROD: "https://today-nujm46x7ta-ue.a.run.app",
 };
 
