56282d821c483087238652329969c3fdc0df5757
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-08-11 13:06:14 -0700

    fix entree

--- extension/src/config.tsx
+++ extension/src/config.tsx
@@ -3,7 +3,5 @@ const config = {
   DEV: "http://127.0.0.1:8000",
   PROD: "https://today-nujm46x7ta-ue.a.run.app",
 }; // TODO change to enum
-// TODO define enum for localstorage keys
-// TODO define enum for tracking events
 
 export default config;
