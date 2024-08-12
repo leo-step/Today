1d517d21bb6ecdc6f82784326d348b55dd5cd0a3
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-08-11 22:07:53 -0700

    set prod url

--- extension/src/config.tsx
+++ extension/src/config.tsx
@@ -1,7 +1,7 @@
 const config = {
   URL: "http://127.0.0.1:8000/api",
   DEV: "http://127.0.0.1:8000/api",
-  PROD: "https://today-nujm46x7ta-ue.a.run.app",
+  PROD: "https://today-fastapi-nujm46x7ta-uc.a.run.app/api",
 }; // TODO change to enum
 
 export default config;
