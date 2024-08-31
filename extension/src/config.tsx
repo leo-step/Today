0591cf2f8d50ab39f1bc7e4ea8b94000f437da6d
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-08-31 14:12:02 -0400

    extension use prod

--- extension/src/config.tsx
+++ extension/src/config.tsx
@@ -1,7 +1,7 @@
 const config = {
-  URL: "http://127.0.0.1:8000",
+  URL: "https://todayapp.org",
   DEV: "http://127.0.0.1:8000",
   PROD: "https://todayapp.org",
-}; // TODO change to enum
+};
 
 export default config;
