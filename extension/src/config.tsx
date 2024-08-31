e3a869963b83f2a1fc4621c21f8e916bdb2481d1
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-08-31 11:43:10 -0400

    point to custom domain

--- extension/src/config.tsx
+++ extension/src/config.tsx
@@ -1,7 +1,7 @@
 const config = {
   URL: "http://127.0.0.1:8000",
   DEV: "http://127.0.0.1:8000",
-  PROD: "https://today-app-5df38c20bbaf.herokuapp.com",
+  PROD: "https://todayapp.org",
 }; // TODO change to enum
 
 export default config;
