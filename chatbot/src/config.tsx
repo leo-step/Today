1fa50bf292de16735599b5a4e67efca67b0c4429
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-10-02 23:15:58 -0400

    implement some cas auth

--- chatbot/src/config.tsx
+++ chatbot/src/config.tsx
@@ -1,6 +1,6 @@
 const config = {
     URL: "https://tay.tigerapps.org",
-    DEV: "http://127.0.0.1:5000",
+    DEV: "http://127.0.0.1:6001",
     PROD: "https://tay.tigerapps.org",
   };
   
--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify, send_from_directory, Response
+from flask import Flask, request, jsonify, send_from_directory, \
+    session, redirect, url_for, abort
 from flask_cors import CORS
 from dotenv import load_dotenv
 from mixpanel import Mixpanel
@@ -8,25 +9,47 @@ from models import Event, ChatQueryInput
 from tools import tools, choose_tool_and_rewrite, invoke_tool
 from response import generate_response
 from memory import Memory, ToolInvocation, MessageType
+from cas import CASClient
 import os
 import uuid
 
 load_dotenv()
 
 app = Flask(__name__, static_folder='dist', static_url_path='/')
+app.secret_key = os.getenv("APP_SECRET_KEY")
+
 mp = Mixpanel(os.getenv("MIXPANEL"))
 
 CORS(app, resources={r"/*": {"origins": "*"}})
+
+cas_client = CASClient()
+
+def authenticated(request):
+    if request.args.get('uuid'):
+        return True
+    return cas_client.is_logged_in()
 
 # ========== UI ==========
 
 @app.route('/static/<path:filename>')
 def serve_static(filename):
+    # if not authenticated(request):
+    #     abort(401)
     return send_from_directory('dist', filename)
 
 @app.route('/')
 def index():
+    if not authenticated(request):
+        return redirect(url_for("login"))
     return send_from_directory('dist', 'index.html')
+
+@app.route("/login")
+def login():
+    cas_client.authenticate()
+    next = url_for("index")
+    if "next" in session:
+        next = session.pop("next")
+    return redirect(next)
 
 # ========== EXTENSION ==========
 
@@ -48,6 +71,8 @@ def track():
 
 @app.route('/api/chat', methods=['POST'])
 def chat():
+    if not authenticated(request):
+        abort(401)
     data = request.get_json()
     query = ChatQueryInput(**data)
     memory = Memory(query.uuid, query.session_id)
@@ -98,4 +123,4 @@ def ios_chat():
     return jsonify({"response": full_response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="localhost", port=6001, debug=True)
