9ed552c0bf3bd79ef9866a0b83a4dff4f2e835a4
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2024-10-17 12:20:24 -0400

    capacitor config

---
+++ chatbot/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.tigerapps.tay',
+  appName: 'Tay',
+  webDir: '../server/dist'
+};
+
+export default config;
